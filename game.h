5f2751236b00503699ef7f3f524aff5f64e420f0
jar3203 <jar3203@rit.edu>
2025-11-04 13:02:52 -0500

    Initial Classes
    Item class done, Enemy class getting wrapped up

--- game.h
+++ game.h
@@ -5,12 +5,26 @@
 using namespace std;
 
 class Item {
-  /*TODO: write your code*/
+private:
+  string name;
+  string desc;
+public:
+  Item(string n, string d);
+  Item(){name = "", desc = "";}
+  string getName(void){return name;}
+  string getDesc(void){return desc;}
 };
+Item::Item(string n, string d) : name(n), desc(d){}
 
 
 class Enemy {
-  /*TODO: write your code*/
+private:
+  string name;
+  int health;
+  int attackPower;
+public:
+  Enemy(string n, int h, int a);
+  Enemy(){name = "", health = 0, attackPower = 0;}
 };
 
 
